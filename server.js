bd71ff666091c3e2e92c718706de5b1d7ce0a583
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-03-20 01:06:01 +0100

    added image upload

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("exprress");
+const express = require("express");
+const mongoClient = require("mongodb").MongoClient;
+const path = require("path");
+
+const mongoUrl = "mongodb://localhost:27017";
+global.db = '';
+mongoClient.connect(mongoUrl, {useUnifiedTopology:true},(err,res)=>{
+    if(err){console.log("Database error"); return}
+    db = res.db("company");
+    console.log("Database listening...");
+});
+
+const postUsers = require(path.join(__dirname,"routes","users","post.js"));
 const app = express();
 
+app.post("/users", postUsers);
 
-app.listen(80, err => {
+app.listen(8080, err => {
     if(err){console.log("Server error"); return}
     console.log("server listening")
 });
